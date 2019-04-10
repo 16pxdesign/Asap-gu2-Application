628794ab98243ef3da1d5fe61d70f32747b915c9
Aleksy <16pxdesign@gmail.com>
2019-04-10 03:33:09 +0100

    +First settings for database connection +Address Model

---
+++ Application/Models/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class Address
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public String Flat { get; set; }
+        public String Street { get; set; }
+        public String Postcode { get; set; }
+        public String City { get; set; }
+    }
+}
---
+++ Application/Models/DatabaseModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Models
+{
+    public class DatabaseModel : DbContext
+    {
+        public DatabaseModel() : base()
+        {
+        }
+
+        public DatabaseModel(DbContextOptions<DatabaseModel> options)
+            : base(options)
+        {
+
+        }
+      
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLazyLoadingProxies();
+                optionsBuilder.UseSqlite("Data Source=db.db");
+            }
+
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+
+
+
+
+    }
+}
