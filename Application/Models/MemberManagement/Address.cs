2c9d5c63beadeddd5dd45eb8f1beff06cc3ec3dd
Aleksy <16pxdesign@gmail.com>
2019-04-14 22:52:09 +0100

    test

--- Application/Models/MemberManagement/Address.cs
+++ Application/Models/MemberManagement/Address.cs
@@ -12,6 +12,9 @@ namespace Application.Models
         public string Street { get; set; }
         public string Postcode { get; set; }
         public string City { get; set; }
+        public string test { get; set; }
+
+
   
         //Relations
     }
